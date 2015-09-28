f4fd3a0df14757f8bee28c07216265fce1eb58c2
maurerja <maurerja@rose-hulman.edu>
2015-09-28 08:47:10 -0400

    added project file

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import org.json.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JSONObject obj = new JSONObject("");
+		
+	}
+
+}
---
+++ src/instanceHandler.java
@@ -0,0 +1,98 @@
+
+public class instanceHandler {
+	private String instanceName;
+	private String dueDate;
+	private String priority;
+	private String closeDate;
+	private String instanceStatus;
+	private String createDate;
+	private String name;
+	private String url;
+	private String assignee;
+	private String instanceId;
+	private String status;
+	private String variables;
+	private String processName;
+	private int id;
+	public instanceHandler(){
+		
+	}
+	public String getInstanceName() {
+		return instanceName;
+	}
+	public void setInstanceName(String instanceName) {
+		this.instanceName = instanceName;
+	}
+	public String getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public String getCloseDate() {
+		return closeDate;
+	}
+	public void setCloseDate(String closeDate) {
+		this.closeDate = closeDate;
+	}
+	public String getInstanceStatus() {
+		return instanceStatus;
+	}
+	public void setInstanceStatus(String instanceStatus) {
+		this.instanceStatus = instanceStatus;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getAssignee() {
+		return assignee;
+	}
+	public void setAssignee(String assignee) {
+		this.assignee = assignee;
+	}
+	public String getInstanceId() {
+		return instanceId;
+	}
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getProcessName() {
+		return processName;
+	}
+	public void setProcessName(String processName) {
+		this.processName = processName;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
